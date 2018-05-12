40ed6927665375ded6863c44e2a57f3854f96531
qcymkxyc <qcymkxyc@163.com>
2018-05-12 17:56:27 +0800

    完成模型的训练

--- lfm.py
+++ lfm.py
@@ -1,83 +1,134 @@
-#! usr/bin/python3
 # coding=utf8
 
 import tensorflow as tf
 from usercf import UserCF
 import pandas as pd
+import sys
+import random
+import numpy as np
+import math
 
 filepath = "store/LFM"
 
-class LFM(object):
+class LFM(UserCF):
     """隐语义模型"""
-    def __init__(self,data,k,learning_rate,regularization_rate):
-#         self.model = tf.estimator.Estimator()
+    def __init__(self,k,learning_rate = 0.01,regularization_rate = 0.1):
+        """
+            @param k:要分成的类别
+            @param learning_rate: 梯度下降的学习率
+            @param regularization_rate:   正则化率   
+        """
         self.k = k
-        self.trainset = data
-        self.testset = None
         self.regularization_rate = regularization_rate
         self.learning_rate = learning_rate
+        super().__init__()
         
+        #读取数据
+        self.read_data(pivot = 0.05);
+        #获取商品集合
+        self.item_pool = self.__get_items(self.trainset)
+        #建立隐语义矩阵
+        self.__build_matrix()
+        
+        
+    def __get_items(self,trainset):
+        """返回所有商品集"""
+        print("开始计算商品集.....",file = sys.stderr)
+        items_pool = set()
+        for user,items in trainset.items():
+            for item in items.keys():
+                items_pool.add(item)
+        print("商品集计算完成",file = sys.stderr)
+        return list(items_pool)
     
-    def load_file(self):
-        dataset = tf.data.TextLineDataset("./data/ml-1m/ratings.dat")
-        print(dataset)
-        return dataset
-    
+    def __build_matrix(self):
+        """建立隐语义矩阵"""
+        print("开始建立隐语义矩阵....",file = sys.stderr)
+        n_users,n_movies = len(self.trainset),len(self.item_pool)
+        #建立用户的隐语义
+        self.user_p = dict()
+        for user in self.trainset.keys():
+            self.user_p[user] = np.random.normal(size = (self.k))
+         
+        self.movie_q = dict()
+        for movie in self.item_pool:
+            self.movie_q[movie] = np.random.normal(size = (self.k))
+        print("隐语义矩阵建立完成",file = sys.stderr)
+            
     def _model_fn(self,n_users,n_movies):
         """主体函数"""
-        p = tf.Variable(
-                    initial_value = tf.random_normal(shape = (n_users,self.k)),
-                    name = "p")
-        q = tf.Variable(
-                    initial_value = tf.random_normal(shape = (self.k,n_movies)),
-                    name = "q")
-        pred_y = tf.matmul(p, q)
-        
-#         loss = tf.reduce_mean(tf.square(self.trainset - pred_y) + \
-#                               self.regularization_rate * p * p + \
-#                               self.regularization_rate * q * q)
-        loss = tf.reduce_mean(tf.square(self.trainset - pred_y))
-        self.loss = loss
-        
-        optimizer = tf.train.GradientDescentOptimizer(self.learning_rate)
-        train_op = optimizer.minimize(loss,global_step = tf.train.get_global_step())
-        return train_op
-    
-    def select_negatives(self,data):
-        """采集负样本"""
+        user_p = np.random.normal(size = (n_users,))
         
         
-    def train(self):
-        train_op = self._model_fn(1, 1);
-        with tf.Session() as session:
-            session.run(tf.initialize_all_variables())
-            for i in range(1000):
-                session.run(train_op)
-                if i % 100 == 0:
-                    print(session.run(self.loss))
-    
-def read_data():
-    data = dict()
-    for i,line in enumerate(UserCF.loadfile()):
-        user, movie, rating, _ = line.split('::')
-        data.setdefault(movie,{})
-        data[movie][user] = rating
-    df = pd.DataFrame(data)
-    df.to_csv("a.csv",index = True)
+    def select_negatives(self,user_movies):
+        """
+            采集负样本,使负样本数量和正样本相同
+            @param user_movies: 用户对应的正样本
+            @return: 正负样本 
+        """
+        items = dict()
+        #采集正样本
+        for movie,rating in user_movies.items():
+            items[movie] = rating
+            
+#         print("开始采集负样本...")
+        #采集负样本
+        n_negative = 0;
+        while n_negative < len(user_movies):
+            negitive_sample = random.choice(self.item_pool)
+            if negitive_sample in items:
+                continue
+            items[negitive_sample] = 0
+            n_negative += 1
+        
+        return items
+      
+    def loss(self):
+        C = 0.;
+        for user,user_latent in self.user_p.items():
+            for movie,movie_latent in self.movie_q.items():
+                try:
+                    rui = self.trainset[user][movie]
+                except KeyError:
+                    rui = 0
+                eui = rui - self.predict(user,movie)
+                C += (np.square(eui) + 
+                      self.regularization_rate * np.sum(np.square(self.user_p[user])) +
+                      self.regularization_rate * np.sum(np.square(self.movie_q[movie]))
+                      ) 
+        return C
+        
+    def predict(self,user,item):  
+        return np.dot(self.user_p[user],self.movie_q[item])
+        
+    def train(self,epoches):
+        for epoch in range(epoches):
+            print("开始第{}轮训练".format(epoch))
+            for user,user_movies in self.trainset.items():
+                select_samples = self.select_negatives(user_movies)
+                for movie,rui in select_samples.items():
+                    eui = rui - self.predict(user,movie)
+                    user_latent = self.user_p[user]
+                    movie_latent = self.movie_q[movie]
+                    
+                    self.user_p[user] += (self.learning_rate * 
+                                          (movie_latent - self.regularization_rate * user_latent))
+                    self.movie_q[movie] += (self.learning_rate * (
+                                            user_latent - self.regularization_rate * movie_latent))
+            print("第{}轮完成".format(epoch))
+       
+        loss = self.loss()       
+        print("loss : {}".format(loss))   
+     
+#      def recommend(self,user):
+#          rank = dict()
+#          for item,qfi in self.movie_q.items():
+#              rank[item] = 
+                 
                         
 if __name__ == "__main__":
-#     read_data()
-    data = pd.read_csv("a.csv")
-    data.fillna(0,inplace = True)
-    
-    lfm = LFM(
-        data = data.values,
-        k = 5,
-        learning_rate = 0.1,
-        regularization_rate = 0.1)
-    
-    lfm.train()
-#     lfm.load_file()
+    lfm = LFM(10)
+    lfm.train(3)
     
 
     
